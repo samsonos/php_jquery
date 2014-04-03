15f80663bda049142e10b64abdf0ca86a4022d2d
Vitaly Iegorov <egorov@samsonos.com>
2014-04-03 19:19:19 +0300

    Init

---
+++ samson.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Интерфейс для подключения модуля в ядро фреймворка SamsonPHP
+ *
+ * @package SamsonPHP
+ * @author Vitaly Iegorov <vitalyiegorov@gmail.com>
+ * @author Nikita Kotenko <nick.w2r@gmail.com>
+ * @version 0.1
+ */
+class JQueryConnector extends samson\core\CompressableExternalModule
+{
+	protected $id = 'jquery'; 
+}
+?>
